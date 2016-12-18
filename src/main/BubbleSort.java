8a921dded791ab9bd9e2f9d6c2aaaefdffbc477c
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-12-18 00:00:24 -0600

    Wrote buble sort program

---
+++ src/main/BubbleSort.java
@@ -0,0 +1,46 @@
+package main;
+
+/**
+ * Created by HarshPatil on 12/17/16.
+ */
+public class BubbleSort {
+
+    public static void main (String args[]) throws Exception{
+
+        int inputArray[] = {2,3,1,-5,4,8};
+
+        int arrayLength = inputArray.length;
+        System.out.print("Input Array ::: {");
+        for(int i=0; i<arrayLength; i++){
+
+            System.out.print(inputArray[i]);
+            if(i!=arrayLength-1){
+                System.out.print(",");
+            }
+        }
+        System.out.print("}\n");
+
+        for(int i=0; i<arrayLength-1; i++){
+
+            for(int j=0; j<arrayLength-i-1; j++){
+
+                if(inputArray[j] > inputArray [j+1]){
+
+                    int temp = inputArray[j+1];
+                    inputArray[j+1] = inputArray[j];
+                    inputArray[j] = temp;
+                }
+            }
+        }
+
+        System.out.print("Sorted Array ::: {");
+        for(int i=0; i<arrayLength; i++){
+
+            System.out.print(inputArray[i]);
+            if(i!=arrayLength-1){
+                System.out.print(",");
+            }
+        }
+        System.out.print("}");
+    }
+}
