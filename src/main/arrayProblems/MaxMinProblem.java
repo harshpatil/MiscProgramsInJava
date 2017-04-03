130d796d3e0fb8d69dcaa2b302a4ce98042715a5
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-04-03 14:32:13 -0500

    Pushing program on stack

--- src/main/arrayProblems/MaxMinProblem.java
+++ src/main/arrayProblems/MaxMinProblem.java
@@ -1,26 +1,31 @@
 package main.arrayProblems;
 
 /**
+ *
+ * Write an efficient function that takes stock_prices_yesterday
+ * and returns the best profit I could have made
+ * from 1 purchase and 1 sale of 1 Apple stock yesterday.
+ *
  * Created by HarshPatil on 4/2/17.
  */
 public class MaxMinProblem {
 
-    static int array[] = {10, 7, 5, 8, 11, 9};
+    static int array[] = {10, 7, 5, 8, 11, 9, 15};
+//    static int array[] = {10, 8, 6, 4, 3, 2};
 
     public static void main(String args[]){
 
-        int max=array[0];
         int min=array[0];
+        int profitAmount = array[1] - array[0];
 
         for(int i=1; i<array.length; i++){
+            if(profitAmount < (array[i] - min)){
+                profitAmount = (array[i]-min);
+            }
             if(array[i] < min){
                 min = array[i];
             }
-            if(array[i] > max){
-                max = array[i];
-            }
         }
-
-        System.out.println("Smallest element:"+ min + " & largest element:"+max);
+        System.out.println("Best stock purchase amount:"+ min + " & selling amount:" + (min+profitAmount) + ", Therefore profit="+profitAmount);
     }
 }
