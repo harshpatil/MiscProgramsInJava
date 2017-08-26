dd076112ce142aff33d64fee7ab412dc0f924b6b
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-08-26 17:04:45 -0500

    Tested scanner for IO

--- src/main/IOInJava.java
+++ src/main/IOInJava.java
@@ -3,6 +3,7 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * Created by HarshPatil on 10/28/16.
@@ -16,5 +17,19 @@ public class IOInJava {
         String input = bufferedReader.readLine();
 
         System.out.println("You entered ::: " + input);
+
+        System.out.println("Enter input ::: ");
+        Scanner scanner = new Scanner(System.in);
+        String scannerInput = scanner.nextLine();
+        System.out.println("You entered ::: " + scannerInput);
+
+        System.out.println("Enter input ::: ");
+        Scanner scanner2 = new Scanner(System.in);
+        while (scanner2.hasNext()){
+            String scannerIn = scanner2.next();
+            System.out.println("You entered ::: " + scannerIn);
+            // This always gets stuck here. Look into it.
+        }
+        scanner.close();
     }
 }
