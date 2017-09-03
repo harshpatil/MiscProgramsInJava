7587732016f9e9ea4f4a7a318eeec91093629378
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-09-03 14:36:09 -0500

    Wrote program for RemoveAllDuplicatesFromAGivenString

---
+++ src/main/dynamicProgramming/MaximumSumRectangleInA2DMatrix.java
@@ -0,0 +1,27 @@
+package main.dynamicProgramming;
+
+/**
+
+    Given a 2D array, find the maximum sum subarray in it.
+    For example, in the following 2D array, the maximum sum subarray is highlighted with blue
+                rectangle and sum of this subarray is 29
+
+    Input:
+        1    2   -1   -4   -20
+        -8  -3    4    2    1
+        3    8    10   1    3
+        -4  -1    1    7    -6
+
+    Output:
+        -3   4   2
+         8   10  1
+        -1   1   7
+
+    http://www.geeksforgeeks.org/dynamic-programming-set-27-max-sum-rectangle-in-a-2d-matrix/
+
+ * Created by HarshPatil on 9/3/17.
+ */
+public class MaximumSumRectangleInA2DMatrix {
+
+
+}
