9f3a028d12317b0f8238ef32b61ff5e0313322e7
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-09-03 19:43:41 -0500

    Wrote program for CheckIfAGivenStringIsPalindrome

---
+++ src/main/trie/GivenASequenceOfWordsPrintAllAnagramsTogether.java
@@ -0,0 +1,17 @@
+package main.trie;
+
+/**
+
+    http://www.geeksforgeeks.org/given-a-sequence-of-words-print-all-anagrams-together-set-2/
+
+    Given an array of words, print all anagrams together.
+    Example,
+        if the given array is {“cat”, “dog”, “tac”, “god”, “act”},
+        then output may be {“cat tac act dog god”}.
+
+ * Created by HarshPatil on 9/3/17.
+ */
+public class GivenASequenceOfWordsPrintAllAnagramsTogether {
+
+    // Complete this
+}
