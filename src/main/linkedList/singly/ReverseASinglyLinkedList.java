be87438121fc2704d0aa95dd86ac82d73136ae18
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-09-06 23:16:30 -0500

    Wrote program for ReverseASinglyLinkedList

---
+++ src/main/linkedList/singly/ReverseASinglyLinkedList.java
@@ -0,0 +1,44 @@
+package main.linkedList.singly;
+
+public class ReverseASinglyLinkedList {
+
+    public static void main(String []args){
+
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new SinglyLinkedListNode(1);
+        sll.head.next = new SinglyLinkedListNode(2);
+        sll.head.next.next = new SinglyLinkedListNode(3);
+        sll.head.next.next.next = new SinglyLinkedListNode(4);
+        sll.head.next.next.next.next = new SinglyLinkedListNode(5);
+
+        printElementsOfLinkedList(sll);
+
+        reverseTheLinkedList(sll);
+        System.out.println("\n\nList after reversal");
+        printElementsOfLinkedList(sll);
+    }
+
+    private static void reverseTheLinkedList(SinglyLinkedList sll) {
+
+        SinglyLinkedListNode prev = null;
+        SinglyLinkedListNode current = sll.head;
+        SinglyLinkedListNode temp = null;
+        while (current != null){
+            temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        sll.head = prev;
+    }
+
+    private static void printElementsOfLinkedList(SinglyLinkedList sll) {
+
+        SinglyLinkedListNode current = sll.head;
+        while (current != null){
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+    }
+
+}
