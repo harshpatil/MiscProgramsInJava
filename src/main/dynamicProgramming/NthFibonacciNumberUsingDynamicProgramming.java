0e3a2aaf32606836584d2959549b3b6ba386c3ce
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-09-12 13:52:58 -0500

    Wrote nth fibonacci program using Tabulation

--- src/main/dynamicProgramming/NthFibonacciNumberUsingDynamicProgramming.java
+++ src/main/dynamicProgramming/NthFibonacciNumberUsingDynamicProgramming.java
@@ -6,21 +6,22 @@ package main.dynamicProgramming;
 public class NthFibonacciNumberUsingDynamicProgramming {
 
     static int nthFiboNumber = 40;
-    static int[] lookup = new int[100];
+    static int[] lookup = new int[nthFiboNumber+1];
 
     public static void main(String[] args){
         NthFibonacciNumberUsingDynamicProgramming nthFibonacciNumber = new NthFibonacciNumberUsingDynamicProgramming();
         nthFibonacciNumber.initializeLookup();
-        System.out.println(nthFibonacciNumber.nthFiboNumber + "th Fibonacci number  :: "+ findNthFiboNumber(nthFibonacciNumber.nthFiboNumber));
+        System.out.println(nthFibonacciNumber.nthFiboNumber + "th Fibonacci number using Memoization :: "+ findNthFiboNumberUsingMemoization(nthFibonacciNumber.nthFiboNumber));
+        System.out.println(nthFibonacciNumber.nthFiboNumber + "th Fibonacci number using Tabulation :: "+ findNthFiboNumberUsingTabulation(nthFibonacciNumber.nthFiboNumber));
 
     }
 
-    private static int findNthFiboNumber(int nthFiboNumber) {
+    private static int findNthFiboNumberUsingMemoization(int nthFiboNumber) {
         if(lookup[nthFiboNumber] == -1){
             if(nthFiboNumber <= 1){
                 lookup[nthFiboNumber] = nthFiboNumber;
             } else {
-                lookup[nthFiboNumber] = findNthFiboNumber(nthFiboNumber-1) + findNthFiboNumber(nthFiboNumber -2);
+                lookup[nthFiboNumber] = findNthFiboNumberUsingMemoization(nthFiboNumber-1) + findNthFiboNumberUsingMemoization(nthFiboNumber -2);
             }
         }
         return lookup[nthFiboNumber];
@@ -28,8 +29,18 @@ public class NthFibonacciNumberUsingDynamicProgramming {
 
     private static void initializeLookup() {
 
-        for (int i=0; i<100; i++){
+        for (int i=0; i<lookup.length; i++){
             lookup[i] = -1;
         }
+    }
+
+    private static int findNthFiboNumberUsingTabulation(int nthFiboNumber) {
+        int[] lookup = new int[nthFiboNumber+1];
+        lookup[0] = 0;
+        lookup[1] = 1;
+        for(int i=2; i<lookup.length; i++){
+            lookup[i] = lookup[i-1] + lookup[i-2];
+        }
+        return lookup[nthFiboNumber];
     }
 }
