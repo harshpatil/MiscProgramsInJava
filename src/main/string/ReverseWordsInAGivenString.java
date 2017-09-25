d3c6f8ea5ecabe1e983e20bec0626c00f00ebc58
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-09-24 23:48:27 -0500

    Wrote program for ReverseWordsInAGivenString

---
+++ src/main/string/ReverseWordsInAGivenString.java
@@ -0,0 +1,28 @@
+package main.string;
+
+/*
+
+    Example: Let the input string be “i like this program very much”.
+    The function should change the string to “much very program this like i”
+
+    Input  : s = "geeks quiz practice code"
+    Output : s = "code practice quiz geeks"
+
+    Input  : s = "getting good at coding needs a lot of practice"
+    Output : s = "practice of lot a needs coding at good getting"
+
+    http://www.geeksforgeeks.org/reverse-words-in-a-given-string/
+ */
+public class ReverseWordsInAGivenString {
+
+    public static void main(String[] args){
+
+        String inputString = "i like this program very much";
+        reverseAndPrint(inputString);
+    }
+
+    private static void reverseAndPrint(String inputString) {
+
+
+    }
+}
