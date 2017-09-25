2b62adba634ce0a30b9d3e0cb71f016279ad5326
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-09-25 00:27:32 -0500

    Changed input String

--- src/main/string/ReverseWordsInAGivenString.java
+++ src/main/string/ReverseWordsInAGivenString.java
@@ -14,13 +14,13 @@ import java.util.regex.Pattern;
     Output : s = "practice of lot a needs coding at good getting"
 
     http://www.geeksforgeeks.org/reverse-words-in-a-given-string/
-    
+
  */
 public class ReverseWordsInAGivenString {
 
     public static void main(String[] args){
 
-        String inputString = "i like this program very much";
+        String inputString = "I am building passion for Algorithms";
         reverseAndPrint(inputString);
     }
 
