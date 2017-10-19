62b44cefbf24b9da78b75324731a7981fa77e692
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-10-18 20:22:21 -0500

    Wrote program for KthLargestElementInBST

---
+++ src/main/tree/binarySearchTree/KthLargestElementInBST.java
@@ -0,0 +1,76 @@
+package main.tree.binarySearchTree;
+
+import main.array.BinarySearch;
+
+/*
+    http://www.geeksforgeeks.org/kth-largest-element-in-bst-when-modification-to-bst-is-not-allowed/
+
+    Given a Binary Search Tree (BST) and a positive integer k,
+    find the k’th largest element in the Binary Search Tree.
+
+ */
+public class KthLargestElementInBST {
+
+    public static void main(String[] args){
+
+        /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80  */
+
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.root = insertNode(null, 50);
+        insertNode(tree.root, 30);
+        insertNode(tree.root, 20);
+        insertNode(tree.root, 40);
+        insertNode(tree.root, 70);
+        insertNode(tree.root, 60);
+        insertNode(tree.root, 80);
+
+        int kth = 4;
+        findKthLargest(tree.root, kth);
+    }
+
+    static public class Count{
+        int count = 0;
+    }
+
+    private static void findKthLargest(BSTNode root, int kth) {
+
+        Count count = new Count();
+        findKthLargestUtil(root, kth, count);
+    }
+
+    private static void findKthLargestUtil(BSTNode node, int k, Count count) {
+
+        if(node == null || count.count >= k){
+            return;
+        }
+        findKthLargestUtil(node.right, k, count);
+        count.count++;
+        if(count.count == k){
+            System.out.println(k + "th Largest element is " + node.data);
+            return;
+        }
+        findKthLargestUtil(node.left, k, count);
+    }
+
+
+    private static BSTNode insertNode(BSTNode root, int key) {
+
+        if(root == null){
+            root = new BSTNode(key);
+            return root;
+        }
+        if(key < root.data){
+            root.left = insertNode(root.left, key);
+        }
+        else {
+            root.right = insertNode(root.right, key);
+        }
+        return root;
+    }
+
+}
