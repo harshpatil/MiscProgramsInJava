e07285148d30641a6fa68d3a4edaee6fb41d1ae1
harshpatil <patil.bmsce@gmail.com>
2017-11-30 14:46:54 -0600

    Wrote program for CountInversionsInAnArray

--- src/main/array/CountInversionsInAnArray.java
+++ src/main/array/CountInversionsInAnArray.java
@@ -9,12 +9,34 @@ package main.array;
         two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j
 
     Example:
-    The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3).
+    The sequence 2, 4, 1, 3, 5 has 3 inversions (2, 1), (4, 1), (4, 3).
+
+    http://www.geeksforgeeks.org/counting-inversions/
+
  */
 public class CountInversionsInAnArray {
 
     public static void main(String[] args){
 
-        // Complete this
+        // With brute force
+        int[] input = {2, 4, 1, 3, 5};
+        findInversionCount(input);
+
+        // Write function with better complexity
+
+    }
+
+    private static void findInversionCount(int[] input) {
+
+        int count=0;
+        for(int i=0; i<input.length; i++){
+            for(int j=i+1; j<input.length; j++){
+                if(input[i] > input[j]){
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Number of inversions = " + count);
     }
 }
