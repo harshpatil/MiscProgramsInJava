d2342a60f85c4f235f46775ca7fd59021cb7057d
harshpatil <patil.bmsce@gmail.com>
2018-08-22 14:19:46 -0700

    Remove extra code

--- src/main/matrix/BooleanMatrixQuestion.java
+++ src/main/matrix/BooleanMatrixQuestion.java
@@ -59,14 +59,6 @@ public class BooleanMatrixQuestion {
         int[] columnArray = new int[column];
 
         for(int i=0; i<row; i++){
-            rowArray[i] = 0;
-        }
-
-        for(int i=0; i<column; i++){
-            columnArray[i] = 0;
-        }
-
-        for(int i=0; i<row; i++){
             for(int j=0; j<column; j++){
                 if(inputMatrix[i][j] == 1){
                     rowArray[i] = 1;
