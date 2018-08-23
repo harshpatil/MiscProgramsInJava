cb61e762f8cacbe9c7d51ba794e113eaf316d04e
harshpatil <patil.bmsce@gmail.com>
2018-08-23 13:33:00 -0700

    Wrote program for SearchAnElementInSortedRotatedArray

--- src/main/Practice.java
+++ src/main/Practice.java
@@ -1,7 +1,55 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by HarshPatil on 8/17/18.
  */
 public class Practice {
+
+    public static void main(String[] args){
+        List<Integer> andrea = new ArrayList<>();
+        andrea.add(1);
+        andrea.add(2);
+        andrea.add(3);
+
+        List<Integer> maria = new ArrayList<>();
+        maria.add(2);
+        maria.add(1);
+        maria.add(3);
+        System.out.println(winner(andrea, maria, "Odd"));
+
+    }
+
+    private static String winner(List<Integer> andrea, List<Integer> maria, String s) {
+        int andreaScore = 0;
+        int mariaScore = 0;
+        if(s.equals("Even")){
+            int i=0;
+            while(i<andrea.size()){
+                andreaScore = andreaScore + (andrea.get(i) - maria.get(i));
+                mariaScore = mariaScore + (maria.get(i) - andrea.get(i));
+                i=i+2;
+            }
+        }
+        else if(s.equals("Odd")){
+            int i=1;
+            while(i<andrea.size()){
+                andreaScore = andreaScore + (andrea.get(i) - maria.get(i));
+                mariaScore = mariaScore + (maria.get(i) - andrea.get(i));
+                i=i+2;
+            }
+        }
+        if(andreaScore == mariaScore){
+            return "Tie";
+        } else if(andreaScore > mariaScore){
+            return "Andrea";
+        } else {
+            return "Maria";
+        }
+    }
+
 }
+
+
--- src/main/array/SearchAnElementInSortedRotatedArray.java
+++ src/main/array/SearchAnElementInSortedRotatedArray.java
@@ -5,41 +5,84 @@ import java.io.InputStreamReader;
 
 /*
     http://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
+    https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
  */
 public class SearchAnElementInSortedRotatedArray {
 
-    static int array[] = {16, 20, 22, 45, 55, 2, 8, 10, 15};
+    public static void main(String []args) throws Exception {
 
-    public static void main(String []args) throws Exception{
+        int[] array = {16, 20, 22, 45, 55, 2, 8, 10, 15};
+        int searchItem = 10;
+        serachForElementUsingPivot(searchItem, array);
+    }
 
-        System.out.println("Enter the element to be searched");
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String input = bufferedReader.readLine();
-        int searchString = Integer.parseInt(input);
+    private static void serachForElementUsingPivot(int searchItem, int[] array){
 
-        int first=0;
-        int last=array.length-1;
-
-        while (first<=last){
-            int mid=(first+last)/2;
-            if(array[mid] == searchString){
-                System.out.println("Element "+searchString+ " found at position:"+mid);
-                System.exit(0);
+        if(array[0] < array[array.length-1]){
+            System.out.println("Array is not rotated");
+            int result = binarySearch(array, 0, array.length-1, searchItem);
+            if(result != -1){
+                System.out.println(searchItem + " Found at position: " + result);
+                return;
             }
-            if(array[first] <= array[mid]){
-                if(array[first] <= searchString && searchString <= array[mid]){
-                    last = mid-1;
-                } else {
-                    first = mid+1;
+        }
+        else {
+            int pivot = findPivot(array);
+            if(searchItem == array[pivot]){
+                System.out.println(searchItem + " Found at position: " + (pivot+1));
+                return;
+            }else if(searchItem <= array[array.length-1]){
+                int result = binarySearch(array, pivot, array.length-1, searchItem);
+                if(result != -1){
+                    System.out.println(searchItem + " Found at position: " + result);
+                    return;
                 }
             } else {
-                if(array[mid] <= searchString && searchString <= array[last]) {
-                    first = mid+1;
-                } else {
-                    last = mid-1;
+                int result = binarySearch(array, 0, pivot-1, searchItem);
+                if(result != -1){
+                    System.out.println(searchItem + " Found at position: " + result);
+                    return;
                 }
             }
         }
-        System.out.println("Element "+searchString+ " is not present in Array");
+        System.out.println(searchItem + " Not Found");
+    }
+
+    private static int binarySearch(int[] array, int low, int high, int searchElement){
+        int result=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(searchElement == array[mid]){
+                result = mid+1;
+                break;
+            }
+            else if(searchElement < array[mid]){
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return result;
+    }
+
+    private static int findPivot(int[] array){
+        int pivot=-1;
+        int low=0;
+        int high=array.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(array[mid]<array[mid-1]){
+                pivot=mid-1;
+                break;
+            } else if(array[mid] > array[mid+1]){
+                pivot=mid+1;
+                break;
+            } else if(array[mid] < array[high]){
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return pivot;
     }
 }
